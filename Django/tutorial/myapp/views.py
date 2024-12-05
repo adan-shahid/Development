214fc7cdd8107bf7f435b705c250e3c006df290e
Adan <adanshahid.student@gmail.com>
2024-12-05 23:13:37 +0500

    I have learnt How to add models and register them in admin. Also how to access django-admin panel and see our database and perform basic CRUD operations

---
+++ Django/tutorial/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def greeting(request):
+    return HttpResponse("Hello Adan!")
+
+def project(request):
+    return render(request, 'index.html')
