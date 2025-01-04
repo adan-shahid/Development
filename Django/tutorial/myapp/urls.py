a9f829f52284dda1828d6a8e0089b977f877733a
Adan <adanshahid.student@gmail.com>
2025-01-04 22:13:04 +0500

    After the completion of 5th semester, I started again

--- Django/tutorial/myapp/urls.py
+++ Django/tutorial/myapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>', views.project, name='project'),
     path('hello',views.hello),
+    path('create-form/', views.createform, name='create-form'),
     
 ]
