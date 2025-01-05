f1c4f2377ed35c60cb37561cc9c06100058e35b8
Adan <adanshahid.student@gmail.com>
2025-01-05 23:00:58 +0500

    Today, I have completed basic CRUD functionality

--- Django/tutorial/myapp/models.py
+++ Django/tutorial/myapp/models.py
@@ -6,8 +6,8 @@ import datetime
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null = True, blank=True)
-    demo_Link = models.CharField(max_length=2000, null=True, blank= True)
-    source_Link = models.CharField(max_length=2000, null=True, blank= True)
+    demo_link = models.CharField(max_length=2000, null=True, blank= True)
+    source_link = models.CharField(max_length=2000, null=True, blank= True)
 
     tags = models.ManyToManyField('Tag',blank=True)
 
--- Django/tutorial/myapp/urls.py
+++ Django/tutorial/myapp/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     
     path('', views.projects, name="projects"),
     path('project/<str:pk>', views.project, name='project'),
-    path('hello',views.hello),
-    path('create-form/', views.createform, name='create-form'),
+    path('create_form/',views.createForm,name='create_form'),
+    path('update-form/<str:pk>', views.updateProject, name='update-form'),
+    path('delete-form/<str:pk>', views.deleteProject, name='delete-form'),
     
 ]
