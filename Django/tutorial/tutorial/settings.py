71a3f60bf4a72c80061d5d05e0500c5b44270e7e
Adan <adanshahid.student@gmail.com>
2025-01-17 03:02:01 +0500

    Completed till static files

--- Django/tutorial/tutorial/settings.py
+++ Django/tutorial/tutorial/settings.py
@@ -121,6 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/img/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
