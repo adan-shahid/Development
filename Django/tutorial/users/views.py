9f859a9ccd8e302c935cbbc6d578f6dab5624a95
Adan <adanshahid.student@gmail.com>
2025-01-21 21:33:30 +0500

    Social Media links are not displaying on the Profile webpage

--- Django/tutorial/users/views.py
+++ Django/tutorial/users/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from .models import Profile, Skill
 # Create your views here.
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    skills = Skill.objects.all()
+    context = {'profiles':profiles,
+               'skills':skills}
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id = pk)
+    context = {'profile':profile}
+    return render(request, 'users/user-profile.html', context)
