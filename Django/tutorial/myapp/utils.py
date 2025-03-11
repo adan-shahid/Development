5e06957682b637dfdb8fd82f60c84a13267feea5
Adan <adanshahid.student@gmail.com>
2025-03-11 20:57:53 +0500

    paginator view for projects is moved to utils.py file, and completed for projects section. Now we are moving to whatif we want to use this paginator in other pages as well?

--- Django/tutorial/myapp/utils.py
+++ Django/tutorial/myapp/utils.py
@@ -1,5 +1,53 @@
 from .models import Project, Tag
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def paginateProjects(request, projects, results):
+    # Code for pagination starts here.
+    # on one page, it will show 3 projects.
+    #page = 1
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
+# pagination code ends here.
+
+# What if we have 1000 of projects and 100 of buttons show in the pagination. That's not look good.
+# so we want to limit the number of buttons on the page. for this we write our own custom class.
+
+
+# Here this code will show option of 5 pages.
+    leftIndex = (int(page) - 4)
+    if leftIndex < 1:
+        leftIndex = 1
+    rightIndex = (int(page) + 5)
+    if rightIndex > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+
+    custom_range = range(leftIndex,rightIndex)
+
+    return custom_range, projects  
+
+
+
+
+
+
+
+
+
+
+
+
 
 def searchProject(request):
     text = ''
--- Django/tutorial/myapp/views.py
+++ Django/tutorial/myapp/views.py
@@ -5,7 +5,7 @@ import datetime
 
 from .models import Project, Tag
 from .forms import ProjectForm
-from .utils import searchProject
+from .utils import searchProject, paginateProjects
 
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
@@ -36,36 +36,52 @@ def hello(request):
     return render(request, 'single-project.html',{'today':day})
 
 def projects(request):
+
     # page = 'project'
     # age = 10
     projects, text = searchProject(request)
 
+    custom_range, projects = paginateProjects(request, projects, 6)
+
+
+# because the views becomes messy wo we are moving this code
+# to utils.py file
+
 # Code for pagination starts here.
     # on one page, it will show 3 projects.
     #page = 1
-    page = request.GET.get('page')
-    results = 3
-    paginator = Paginator(projects, results)
+#    page = request.GET.get('page')
+#    results = 3
+#    paginator = Paginator(projects, results)
 
-    try:
-        projects = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1
-        projects = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        projects = paginator.page(page)
+
+#    try:
+#        projects = paginator.page(page)
+#    except PageNotAnInteger:
+#        page = 1
+#        projects = paginator.page(page)
+#    except EmptyPage:
+#        page = paginator.num_pages
+#       projects = paginator.page(page)
 # pagination code ends here.
 
 # What if we have 1000 of projects and 100 of buttons show in the pagination. That's not look good.
 # so we want to limit the number of buttons on the page. for this we write our own custom class.
+# Here this code will show option of 5 pages.
+#   leftIndex = (int(page) - 4)
+#    if leftIndex < 1:
+#        leftIndex = 1
+#    rightIndex = (int(page) + 5)
+#   if rightIndex > paginator.num_pages:
+#        rightIndex = paginator.num_pages + 1
 
-    custom_range = range(1,20)
+#    custom_range = range(leftIndex,rightIndex) """
+
 
 
    # we are passing here 'projects', so that it stays in the search bar
-    context = {'projects':projects, 'text':text, 
-             'paginator':paginator, 'custom_range':custom_range}
+    context = {'projects':projects, 'text':text,
+              'custom_range':custom_range}
         
     # context = {'page':page, 'age':age, 'projects':projectsList}
     return render(request,'index.html', context)
