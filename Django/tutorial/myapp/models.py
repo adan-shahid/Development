818d922f82bb1d203e2005f6561335a00ffc29a4
Adan <adanshahid.student@gmail.com>
2025-03-13 22:41:50 +0500

    Started learning about Reviews. Only One review and by one user

--- Django/tutorial/myapp/models.py
+++ Django/tutorial/myapp/models.py
@@ -35,13 +35,17 @@ class Review(models.Model):
         ('down','Down Vote'),
     )
 
-    #owner
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null = True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE) # when the model is deleted, all the reviews should also be deleted.
     body = models.TextField(null = True, blank=True)
     value = models.CharField(max_length=200, choices = VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+# WE DONT WANT A SINGLE USER TO LEAVE MORE THAN ONE COMMENT.
+
+    class Meta:
+      unique_together = [['owner', 'project']]
 
     def __str__(self):
         return self.value
